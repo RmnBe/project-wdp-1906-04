df4b070a166fafde6436a8c864ff54b5523fa8a5
Anna Langner <annalangner06@gmail.com>
2019-06-30 22:38:28 +0200

    Add functionality to move between tabs

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,13 @@
 console.log('hello');
+
+// section display
+
+var bookmarks = document.getElementById('bookmarks');
+
+bookmarks.addEventListener('click', function (event) {
+  event.preventDefault();
+  var elem = event.target.tagName === 'a' ? event.target.parentElement : event.target;
+  var id = elem.getAttribute('href');
+  document.querySelector('.sectionDisplay.show').classList.remove('show');
+  document.querySelector(id).classList.add('show');
+});
