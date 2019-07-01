de51cb42b4656651f43f75ae9f47f0900aa381dc
Anna Langner <annalangner06@gmail.com>
2019-07-01 11:59:34 +0200

    Adding the "active" class deletion

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -10,4 +10,7 @@ bookmarks.addEventListener('click', function (event) {
   var id = elem.getAttribute('href');
   document.querySelector('.sectionDisplay.show').classList.remove('show');
   document.querySelector(id).classList.add('show');
+  document.querySelector('.sectionActive.active').classList.remove('active');
+  // tu coś nie tak nie wiem jak się dobrać do tego elementu
+  document.querySelector(id).classList.add('active');
 });
