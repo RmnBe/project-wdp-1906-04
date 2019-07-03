59dd5aa9895dcdb457aa2c321d85908addf9da3a
Paweł Żurawka <pzurawka@gmail.com>
2019-07-03 21:33:45 +0200

    Add RWD to Select,Search and menu

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,12 @@
 console.log('hello');
+
+// CHANGE MENU SIZE WHEN CLICK TO HAMBURGER < 576PX
+function toggleMenu (visible) {
+  document.querySelector('body').classList.toggle('active-menu', visible);
+  document.querySelector('#hamburger').classList.toggle('active-menu-btn');
+}
+
+document.querySelector('#hamburger').addEventListener('click', function (i) {
+  i.preventDefault();
+  toggleMenu();
+});
