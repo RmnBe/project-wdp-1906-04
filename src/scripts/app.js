17794fb143b835de334be3c660911c39fcd11ae5
Anna Langner <annalangner06@gmail.com>
2019-07-05 12:49:04 +0200

    Add the implementation of the tiny-slider

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,11 @@
 console.log('hello');
+
+// Implementation of tiny-slider
+
+// eslint-disable-next-line no-unused-vars,no-undef
+const slider = tns({
+  container: '.my-slider',
+  slideBy: 'page',
+  items: 2,
+  autoplay: true
+});
