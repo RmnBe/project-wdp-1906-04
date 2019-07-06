deaad31340e25614437ad7e6a12c245375536ab2
Anna Langner <annalangner06@gmail.com>
2019-07-06 12:27:29 +0200

    Added of carousel settings

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,11 +1,14 @@
-console.log('hello');
-
 // Implementation of tiny-slider
 
 // eslint-disable-next-line no-unused-vars,no-undef
 const slider = tns({
   container: '.my-slider',
-  slideBy: 'page',
+  mode: 'carousel',
   items: 2,
-  autoplay: true
+  autoWidth: true,
+  slideBy: 'page',
+  center: true,
+  speed: 400,
+  autoplay: true,
+  mouseDrag: true
 });
