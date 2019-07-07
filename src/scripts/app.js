c8d19c8d57c37bc43f33f14ecf5cd17fd914a194
Anna Langner <annalangner06@gmail.com>
2019-07-07 17:16:18 +0200

    Addition of the carousel implementation

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,10 @@
-console.log('hello');
+// implementation of the carousel
+// eslint-disable-next-line no-unused-vars,no-undef
+const slider = tns({
+  container: '.my-slider',
+  mode: 'carousel',
+  loop: false,
+  items: 3,
+  slideBy: 'page',
+  autoplay: true
+});
