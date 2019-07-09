8e988641756b63b2ac6574a4163fa7a54ab0a31a
Anna Langner <annalangner06@gmail.com>
2019-07-09 20:44:52 +0200

    Add the implementation of the tiny-slider

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,25 @@
-console.log('hello');
+// eslint-disable-next-line no-unused-vars,no-undef
+const slider = tns({
+  container: '.my-slider',
+  mode: 'carousel',
+  loop: false,
+  items: 1,
+  slideBy: 'page',
+  autoWidth: true,
+  speed: 400,
+  mouseDrag: true,
+  controlsText: ['<', '>'],
+  nav: false,
+  responsive: {
+    410: {
+      item: 2
+    },
+    640: {
+      items: 4
+    },
+
+    768: {
+      items: 6
+    }
+  }
+});
