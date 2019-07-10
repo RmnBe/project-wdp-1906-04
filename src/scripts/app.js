12adcaa4c71ab47cf3a73febce3b379dbf1eef2d
Roman Bednarz <roman.bednarz@gmail.com>
2019-07-10 20:20:51 +0200

    carousel problems

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,14 @@
-console.log('hello');
+// implementation of the carousel
+// eslint-disable-next-line no-unused-vars,no-undef
+const slider = tns({
+  container: document.querySelector('.my-slider'),
+  mode: 'carousel',
+  loop: false,
+  items: 1,
+  slideBy: 'page',
+  autoWidth: true,
+  speed: 400,
+  mouseDrag: true,
+  controlsContainer: document.querySelector('.my-controls'),
+  nav: true
+});
