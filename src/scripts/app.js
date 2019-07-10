53f6a9b4a1e67dc7774fe97e706a8271627e184a
Anna Langner <annalangner06@gmail.com>
2019-07-10 13:56:33 +0200

    Changes the carousel settings

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,16 +1,14 @@
 // implementation of the carousel
 // eslint-disable-next-line no-unused-vars,no-undef
 const slider = tns({
-  container: '.my-slider',
-  mode: 'carousel',
-  loop: false,
+  container: document.querySelector('.my-slider'),
+  autoplay: true,
+  loop: true,
+  autoplayButton: false,
   items: 1,
-  slideBy: 'page',
-  autoWidth: true,
-  speed: 400,
+  gutter: 20,
+  controls: false,
   mouseDrag: true,
-  controlsText: ['<', '>'],
-  nav: false,
   responsive: {
     410: {
       item: 1
