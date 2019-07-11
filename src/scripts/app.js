46ed0771c0675be572035be8f9191a42c57b815e
Roman Bednarz <roman.bednarz@gmail.com>
2019-07-11 18:49:23 +0200

    promotion-right slider done

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,14 +1,15 @@
 // implementation of the carousel
 // eslint-disable-next-line no-unused-vars,no-undef
 const slider = tns({
-  container: document.querySelector('.my-slider'),
+  container: '.my-slider',
   mode: 'carousel',
   loop: false,
   items: 1,
   slideBy: 'page',
-  autoWidth: true,
+  // autoWidth: true,
   speed: 400,
   mouseDrag: true,
-  controlsContainer: document.querySelector('.my-controls'),
-  nav: true
+  controlsContainer: '.my-controls',
+  // controlText: ['<', '>'],
+  nav: false
 });
