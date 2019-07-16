6ae71ea15d81cd4c8abb021eb8d5d505767b0ad8
Anna Langner <annalangner06@gmail.com>
2019-07-16 14:34:47 +0200

    Add the implementation of the tiny-slider

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -14,3 +14,34 @@ bookmarks.addEventListener('click', function (event) {
   }
   elem.classList.add('active');
 });
+
+// Implementation of carousel
+// eslint-disable-next-line no-unused-vars,no-undef
+const slider = tns({
+  container: document.querySelector('.my-slider'),
+  loop: true,
+  autoplay: true,
+  autoplayButton: false,
+  items: 1,
+  slideBy: 'page',
+  controlsText: ['<', '>'],
+  nav: false,
+  mouseDrag: true,
+  responsive: {
+    370: {
+      item: 1
+    },
+    576: {
+      items: 2
+    },
+    768: {
+      items: 3
+    },
+    992: {
+      items: 5
+    },
+    1200: {
+      items: 6
+    }
+  }
+});
