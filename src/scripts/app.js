2d73581b0cff0de0aad44c35daf1189123b26652
Anna Langner <annalangner06@gmail.com>
2019-07-19 14:25:45 +0200

    Creating Toggleable Tabs

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -45,3 +45,23 @@ const slider = tns({
     }
   }
 });
+
+// Add script to gallery
+// eslint-disable-next-line no-unused-vars
+function openGallery (evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName('tabcontent');
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = 'none';
+  }
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName('tablinks');
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(' active', '');
+  }
+  // Show the current tab
+  document.getElementById(cityName).style.display = 'block';
+  evt.currentTarget.className += ' active';
+}
